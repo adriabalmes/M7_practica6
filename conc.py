9901971f71e6d2d256a0d2bbfc585ec3067063c6
FHP187 <gjimenez@jaumebalmes.net>
2022-11-10 19:59:41 +0100

    Creamos con.py input por el usuario

--- conc.py
+++ conc.py
@@ -3,10 +3,19 @@ def function1 (): # definimos la funcion
 
     resultado = x + y # definimos la suma
 
+    print(resultado)
+
     print(f"La suma de {x} y {y} es: {resultado}") # print de la suma
     return
 
-x = 1 # variable x
-y = 2 # variable y
+print("Valor de la variable x")
+x = input()
+x = int(x)
+
+print("Valor de la variable y")
+y = input()
+y = int(y)
+#x = 1 # variable x
+#y = 2 # variable y
 
 function1() # retornamos la function1
