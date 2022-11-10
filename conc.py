e5dbc468cdc6f2e2ef7f14b93e81968a44057149
FHP187 <gjimenez@jaumebalmes.net>
2022-11-10 19:50:59 +0100

    Creamos con.py

---
+++ conc.py
@@ -0,0 +1,12 @@
+
+def function1 (): # definimos la funcion
+
+    resultado = x + y # definimos la suma
+
+    print(f"La suma de {x} y {y} es: {resultado}") # print de la suma
+    return
+
+x = 1 # variable x
+y = 2 # variable y
+
+function1() # retornamos la function1
