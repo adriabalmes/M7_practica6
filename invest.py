09fd5ecb384c0e2be5bd3cfc7fb142b75903d055
adriabalmes <aberruez@jaumebalmes.net>
2022-11-10 19:55:06 +0100

    ejercicio 2

--- invest.py
+++ invest.py
@@ -1,13 +1,4 @@
 def invest (): 
     print (100 * 1.1 * 7 + 100)
 
-invest()
-
-# Crear un arxiu amb nom invest2.py. És identic a l’exercici 2 
-# però utilitzant les següents variables; savings i increase. A 
-# la variable savings s’hi guardaran els diners inicials i a la 
-# variable increase el valor a augmentar cada any. Crear una 
-# variable result que ens donarà el valor final del 
-# càlcul demanat.
-
 
